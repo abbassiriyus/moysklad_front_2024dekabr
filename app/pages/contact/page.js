27c7ca6db55dd02de29e8b95d807f42dc9b38696
otabekbeb <86982819+abbassiriyus@users.noreply.github.com>
2024-12-07 18:35:13 +0500

    qwe
    qew

---
+++ app/pages/contact/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar1 from '../../components/NavbarPage'
+import Footer1 from '../../components/footer'
+export default function page() {
+  return (
+    <div>
+        <Navbar1/>
+
+
+<Footer1 />
+    </div>
+  )
+}
